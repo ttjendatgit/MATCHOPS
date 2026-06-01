3d3b96e97239453425c9d89da92f41f8d120a431
Your Name <you@example.com>
2026-06-01 22:24:52 +0700

    deploy

--- Backend/MATCHOP.API/Program.cs
+++ Backend/MATCHOP.API/Program.cs
@@ -144,11 +144,8 @@ var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
